f112810973d58069b016232cef1a1a427dad98d4
keylor24 <keylorbasoto2405@gmail.com>
2025-08-11 14:25:03 -0600

    Crea función agregarAmigo para capturar, validar y agregar nombre al array de amigos

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 let amigos = [];
 
+function agregarAmigo() {
+    let nuevoAmigo = document.getElementById('amigo').value;
+
+    if(nuevoAmigo.trim() == "") {
+        alert("Por favor, ingresa un nombre válido.");
+    }
+    else{
+        amigos.push(nuevoAmigo);
+    }
+}
+
+
